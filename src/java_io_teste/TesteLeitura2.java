a76710388f734d00656250da863ac585b8bc5cef
diegoMarks <diegomarks1806@gmail.com>
2022-07-25 19:19:57 -0300

    Utilização dos metodos Scanner e Locale para tipo de separação decimal

--- src/java_io_teste/TesteLeitura2.java
+++ src/java_io_teste/TesteLeitura2.java
@@ -2,6 +2,7 @@ package java_io_teste;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -15,8 +16,29 @@ public class TesteLeitura2 {
 		//retorna proxima linha
 		String linha = scanner.nextLine();
 		System.out.println(linha);
-		}
 		
+		//Scanner que recebe a linha para tratamento
+		Scanner linhaScanner = new Scanner(linha);
+		//Define para o sistema utilizar as regras americanas de pontuacao
+		linhaScanner.useLocale(Locale.US);
+		//insere um delimitador na String.
+		linhaScanner.useDelimiter(",");
+		
+		String valor1 = linhaScanner.next();
+		int valor2 = linhaScanner.nextInt();
+		int valor3 = linhaScanner.nextInt();
+		String valor4 = linhaScanner.next();
+		double valor5 = linhaScanner.nextDouble();
+		
+		
+		System.out.println(valor1+valor2+valor3+valor4+valor5);
+		linhaScanner.close();
+		//metodo split quebra uma String, nesse caso esta inserindo uma virgula
+		/*
+		 * String[] valores = linha.split(","); System.out.println(valores[3]);
+		 */
+		}
+		//fechando classe scanner
 		scanner.close();
 	}
 
